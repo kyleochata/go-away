fc841e4230d44b122d11659bd51c6be734bc0a06
kyleochata <kyleochata@gmail.com>
2024-10-07 16:16:14 -0700

    particle system go

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/kyleochata/go-away/particles"
 )
 
+// func main() {
+// 	coffee := particles.NewCoffee(7, 5)
+// 	coffee.Start()
+
+//		timer := time.NewTicker(time.Millisecond * 100.0)
+//		for {
+//			<-timer.C
+//			fmt.Print("\033[H\033[2J") // clear the screen
+//			coffee.Update()
+//			fmt.Println(coffee.Display())
+//		}
+//	}
 func main() {
-	fmt.Println("Hello, World!")
+	coffee := particles.NewCoffee(9, 5)
+	coffee.Start()
+
+	timer := time.NewTicker(time.Millisecond * 100)
+	for {
+		<-timer.C
+		fmt.Print("\033[H") // move cursor to the top without clearing the screen
+		coffee.Update()
+		fmt.Println(coffee.Display())
+	}
 }
